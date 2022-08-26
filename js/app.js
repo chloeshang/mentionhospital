6adb02476a469d3d73d1c6445e915c707a1715e3
chloeshang <chloeshang49@gmail.com>
2022-08-26 11:23:34 +0630

    16 june 2022

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	// console.log('hi');
+
+	// Start Header Section
+
+	// Start Banner Section
+		// $(".carousel").carousel({
+		// 	interval:1000
+		// });
+	// End Banner Section
+
+	// End Header Section
+});
